a29089d5845725d6a7e368c3b3174281b7a438f6
dowonee <sssjdw@naver.com>
2022-02-26 01:06:02 +0900

    spring

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,14 +1,19 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 // 기능을 실행하는 책임만 져야한다.
-@Component("service")
+@Component
+//@RequiredArgsConstructor //final이 붙은것을 가지고 생성자를 만들어줌
 public class OrderServiceImpl implements OrderService{
 
     //DIP를 지키는 방식 => 인터페이스에만 의존
@@ -16,15 +21,19 @@ public class OrderServiceImpl implements OrderService{
     private final DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("memberRepository = " + memberRepository);
-        System.out.println("discountPolicy = " + discountPolicy);
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-    // 생성자있는건 웬만한건 다 값을 다 채워넣어야한다.
-    // 주로 불변이면서 필수인 의존관계
-    // 생성자가 딱 하나만 있으면 @Autowired 생략가능
+
+    // 가끔 생성자가 직접 필요할 때만 직접 만든다.
+
+    // 최근에는 생성자를 딱 하나두고, @Autowired를 생략하는 방법을 주로 사용한다.
+    // 여기에 Lombok 라이브버리의 @RequiredArgsConstructor 를 함께 사용하면 기능은 다 제공하면서 코드는 깔끔하게 사용할 수 있다.
+
+    //@Qualifier("mainDiscountPolicy") 로 주입할때 @Qualifier("mainDiscountPolicy")를 못찾으면
+    //mainDiscountPolicy 라는 이름의 스프링 빈을 추가로 찾는다. 하지만 @Qualifier는 @Qualifier를 찾는 용도로만 사용하는게
+    //명확하고 좋다.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
---
+++ src/test/java/hello/core/liftcycle/NetworkClient.java
@@ -0,0 +1,46 @@
+package hello.core.liftcycle;
+
+
+public class NetworkClient {
+
+    // 스프링 빈의 이벤트 라이프 사이클
+    // 스프링컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종료
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect : " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " meaasge = " + message);
+
+    }
+    // 서비스 종료 시 호출
+    public void disconnect() {
+        System.out.println("close " + url);
+    }
+
+    // 의존관계 주입이 끝나면 호출해주겠다는 뜻
+
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+}
